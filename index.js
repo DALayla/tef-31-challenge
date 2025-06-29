eb313c8825eb593d11024be90a215417ff902853
DALayla <laylakim0311@gmail.com>
2025-06-29 14:38:58 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const App = () => {
-  return <h1>Bienvenue sur le Défi TEF 31 Jours!</h1>;
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
