39943baddd84b70e094c76457bf26bdd653be540
DALayla <laylakim0311@gmail.com>
2025-06-29 14:46:21 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>TEF 31일 작문 연습</h1>
+      <p>연습을 시작하려면 날짜를 선택하세요.</p>
+    </div>
+  );
+}
+
+export default App;
