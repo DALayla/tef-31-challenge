94f186f478dafc5a5ca8f2bd44732269d511f378
DALayla <laylakim0311@gmail.com>
2025-06-29 14:58:45 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // 🔥 이게 없으면 자바스크립트 못 불러올 수 있음
 })
